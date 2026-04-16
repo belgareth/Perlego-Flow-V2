8f72ffc3efd1af81192b76c9defb6bb30fc9322b
Ben T. K <bkanoel5@gmail.com>
2026-04-16 10:16:44 +0300

    Refactor progress bar functions and add new event listener

--- popup.js
+++ popup.js
@@ -1,22 +1,18 @@
-
 function updateProgressBar(progress) {
   const progressBar = document.getElementById('progress');
-  progressBar.style.width = progress + '%';
+  if(progressBar) progressBar.style.width = progress + '%';
 }
-
 
 function showProgressBar() {
   const progressBar = document.getElementById('progressBar');
-  progressBar.style.display = 'block';
+  if(progressBar) progressBar.style.display = 'block';
 }
-
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => { 
   if (message.type === 'progressUpdate') {
     updateProgressBar(message.progress);
   }
 });
-
 
 document.getElementById('executar').addEventListener('click', () => {
   chrome.runtime.sendMessage({ type: 'startScript' });
@@ -25,11 +21,17 @@ document.getElementById('executar').addEventListener('click', () => {
 
 document.getElementById('manual').addEventListener('click', () => {
   chrome.runtime.sendMessage({ type: 'startScriptmanual' });
-	console.log('Manual')
+  console.log('Manual Chunking');
+  showProgressBar();
+});
+
+document.getElementById('manualfull').addEventListener('click', () => {
+  chrome.runtime.sendMessage({ type: 'startScriptmanualfull' });
+  console.log('Manual Full Brute Force');
   showProgressBar();
 });
 
 document.getElementById('clear').addEventListener('click', () => {
   chrome.runtime.sendMessage({ type: 'resetAndStartCapture' });
-	console.log('clear')
+  console.log('clear');
 });
